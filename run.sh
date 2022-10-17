1f7ba85a2beb40a68e15313f6fc92a596277580d
Tife <48148334+yu6315@users.noreply.github.com>
2022-10-17 21:58:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ wget "https://raw.githubusercontent.com/yu6315/ssAuto/main/shell.sh"
 echo "input dpass:"
 read dpass
 export dpass
-openssl enc -aes-128-cbc -pbkdf2 -in shell.sh -a -d -out shell1.sh -pass env:dpass
+openssl enc -aes-128-cbc -in shell.sh -a -d -out shell1.sh -pass env:dpass
 chmod +x shell1.sh
